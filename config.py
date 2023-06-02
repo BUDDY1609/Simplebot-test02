16f03261eaa7b5312859c8d60ae0ab463e497096
gitdrive5 <121691578+gitdrive5@users.noreply.github.com>
2023-06-03 00:48:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
+import re, os
 from os import environ
+
+id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(environ.get("API_ID", ""))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 CAPTION = environ.get("CAPTION", "")
 
 # for thumbnail ( back end is MrMKN brain 😉)
